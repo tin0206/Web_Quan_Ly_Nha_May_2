7c34604e438500db40cf0ca3c254e5220f01244c
tin0206 <ngtrungtin02062005@gmail.com>
2026-02-10 18:20:10 +0700

    Implement docker

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
